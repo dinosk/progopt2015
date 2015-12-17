2813e7fb2be3171cf78fb3e424bc2ce3c14891fd
Dr. Michael Petter <petter@in.tum.de>
2015-12-17 13:55:36 +0100

    with access to expression

--- cfgstructure/src/petter/cfg/edges/MethodCall.java
+++ cfgstructure/src/petter/cfg/edges/MethodCall.java
@@ -11,7 +11,16 @@ import petter.cfg.Visitor;
 public class MethodCall extends Transition {
 
     private final petter.cfg.expression.MethodCall m;
+        
+    /**
+     * obtain the call expression
+     * @return the call expression
+     */
+    public petter.cfg.expression.MethodCall getCallExpression() { 
+        return m; 
+    }
 
+    
     public MethodCall(State source, State dest, petter.cfg.expression.MethodCall m) {
         super(source, dest);
         this.m = m;
