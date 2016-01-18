ef769e764834e0d24ab0449710c5eb967bf95eed
Dr. Michael Petter <petter@in.tum.de>
2016-01-18 16:41:53 +0100

    beginnings of unit tests

---
+++ simplec/tests/petter/simplec/LanguageFeatures.java
@@ -0,0 +1,14 @@
+package petter.simplec;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LanguageFeatures {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
