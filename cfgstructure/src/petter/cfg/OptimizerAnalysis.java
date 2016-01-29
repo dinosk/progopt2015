da7b9aaf8a68341194a26d06ceff6a66ad53ae78
Eirini Psallida <eirini.psallida@gmail.com>
2016-01-29 20:30:47 +0100

    WIP VarVar Moves Analysis

--- cfgstructure/src/petter/cfg/OptimizerAnalysis.java
+++ cfgstructure/src/petter/cfg/OptimizerAnalysis.java
@@ -15,10 +15,23 @@ public class OptimizerAnalysis{
         ArrayList<Procedure> worklist = new ArrayList<Procedure>();
         CallGraphBuilder callGraphBuilder = new CallGraphBuilder(cu);
         TailRecursionAnalysis tr = new TailRecursionAnalysis(cu);
-        ConstantPropagationAnalysis cpa = new ConstantPropagationAnalysis(cu);
-        // CopyPropagationAnalysis copyprop = new CopyPropagationAnalysis(cu);
+        // ConstantPropagationAnalysis cpa = new ConstantPropagationAnalysis(cu);
 
-        
+        VarToVarMoveAnalysis varTovar = new VarToVarMoveAnalysis(cu);
+        System.out.println("------------ Starting VarVarMoveAnalysis 0/4 ------------");
+        Procedure __main = cu.getProcedure("main");
+        varTovar.enter(__main, null);
+        varTovar.fullAnalysis();
+
+        System.out.println("Available Expr: " + varTovar.getAvailableExpr());
+
+        Iterator<Procedure> allmethods = cu.iterator();
+        while(allmethods.hasNext()){
+            Procedure nextProc = allmethods.next();
+            callGraphBuilder.enter(nextProc);
+            tr.enter(nextProc, null);
+            // cpa.enter(nextProc, null);
+        }
 
         ArrayList<Procedure> leafProcs = callGraphBuilder.getLeafProcs();
         HashMap<Procedure, ArrayList<Procedure>> callGraph = callGraphBuilder.getCallGraph();
@@ -36,11 +49,11 @@ public class OptimizerAnalysis{
             procCalls.put(methodName, 0);
         }
         NumOfCallsVisitor callsVisitor = new NumOfCallsVisitor(cu, procCalls);
-        Iterator<Procedure> allmethods = cu.iterator();
+        allmethods = cu.iterator();
         while(allmethods.hasNext()){
             callsVisitor.enter(allmethods.next());
         }
-        //callsVisitor.fullAnalysis();
+        // callsVisitor.fullAnalysis();
         // callsVisitor.enter(__main);
         // callsVisitor.fullAnalysis();
         ArrayList<Procedure> numOfCalls = new ArrayList<Procedure>();
@@ -56,38 +69,28 @@ public class OptimizerAnalysis{
         while(allmethods.hasNext()){
             ia1.enter(allmethods.next());
         }
-		//ia1.fullAnalysis();
-		InliningAnalysis ia = new InliningAnalysis(cu, leafProcs);
-        // ia.fullAnalysis();
-        System.out.println("------------ Starting TailRecursionAnalysis 2/3 ------------");
-        allmethods = cu.iterator();
-        while(allmethods.hasNext()){
-            Procedure nextProc = allmethods.next();
-            callGraphBuilder.enter(nextProc);
-            tr.enter(nextProc, null);
-            // cpa.enter(nextProc, null);
-        }
-        // tr.fullAnalysis();
+        // ia1.fullAnalysis();
 
-        System.out.println("------------ Starting ConstantPropagationAnalysis 3/4 ------------");
-        Procedure __main = cu.getProcedure("main");
-        cpa.enter(__main, null);
-        cpa.fullAnalysis();
+        InliningAnalysis ia = new InliningAnalysis(cu, leafProcs);
 
-        // worklist.add(__main);
-        // cpa.setWorklist(worklist);
-        // //#TODO check if should iterate
-        // while(!worklist.isEmpty()){
-        // 	Procedure nextProc = worklist.get(0);
-        // 	worklist.remove(0);
-        // 	cpa.enter(nextProc, cpa.dataflowOf(nextProc.getEnd()));
-        // 	cpa.fullAnalysis();
+        // System.out.println("------------ Starting InliningAnalysis 1/4 ------------");
+        // allmethods = cu.iterator();
+        // while(allmethods.hasNext()){
+        //     ia.enter(allmethods.next());
         // }
-        
-
+        // ia.fullAnalysis();
+        System.out.println("------------ Starting TailRecursionAnalysis 2/4 ------------");
+        // tr.fullAnalysis();
+        System.out.println("------------ Starting ConstantPropagationAnalysis 3/4 ------------");
+        Procedure bar = cu.getProcedure("bar");
+        worklist.add(bar);
+        //#TODO check if should iterate
         // cpa.enter(bar, null);
         // cpa.fullAnalysis();
 
+        System.out.println("------------ Starting ConstantPropagationAnalysis 4/4 ------------");
+        Procedure ___main = cu.getProcedure("main");
+        worklist.add(___main);
         //#TODO check if should iterate
         // copyprop.enter(___main, null);
         // copyprop.fullAnalysis();
@@ -103,13 +106,24 @@ public class OptimizerAnalysis{
         // allmethods = cu.iterator();
         // while(allmethods.hasNext()){
         //     Procedure proc = allmethods.next();
-        //     DotLayout layout = new DotLayout("jpg", proc.getName()+"After22.jpg");
+        //     DotLayout layout = new DotLayout("jpg", proc.getName()+"After111.jpg");
         //     System.out.println("----------------"+proc.getName()+"----------------");
         //     for (State s: proc.getStates()){
-        //         System.out.println("For "+s+" we have "+cpa.dataflowOf(s));
-        //         layout.highlight(s,(cpa.dataflowOf(s))+"");
+        //         System.out.println("For "+s+" we have "+varTovar.dataflowOf(s));
+        //         layout.highlight(s,(varTovar.dataflowOf(s))+"");
         //     }
         //     layout.callDot(proc);
+        // }
+        //         allmethods = cu.iterator();
+        // while(allmethods.hasNext()){
+        //     Procedure proc = allmethods.next();
+            DotLayout layout = new DotLayout("jpg", __main.getName()+"After111.jpg");
+            System.out.println("----------------"+__main.getName()+"----------------");
+            for (State s: __main.getStates()){
+                System.out.println("For "+s+" we have "+varTovar.dataflowOf(s));
+                layout.highlight(s,(varTovar.dataflowOf(s))+"");
+            }
+            layout.callDot(__main);
         // }
 
     }
---
+++ cfgstructure/src/petter/cfg/test_VarVarMoves.c
@@ -0,0 +1,22 @@
+int main(){
+    int a;
+    int b;
+    int c;
+    int d;
+    int z;
+
+    a = 0;
+    b = 4;
+    c = b * 3 + 8;
+    d = b * 3;
+    z = d;
+    a = 4;
+    if(!a == 0) {
+        z++;
+    }
+    else {
+        z--;
+    }
+
+    return a;
+}
